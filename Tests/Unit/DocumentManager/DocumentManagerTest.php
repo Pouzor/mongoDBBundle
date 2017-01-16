417fc56ce0e9a0ff6cecb846bd25b8ecf590b77a
Pouzor <remy.jardinet@gmail.com>
2017-01-16 01:32:14 +0100

    Architecture for test

---
+++ Tests/Unit/DocumentManager/DocumentManagerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Pouzor\MongoDBBundle\Tests\Unit\Controller;
+
+use Pouzor\MongoDBBundle\DocumentManager\DocumentManager;
+use Psr\Log\NullLogger;
+
+class DocumentManagerTest extends \PHPUnit_Framework_TestCase
+{
+    private $manager = null;
+
+    private $config = [
+        "db" => "mongodbbundle",
+        "host" =>  "localhost",
+        "port" => "27017",
+        "username" =>  null,
+        "password" => null,
+        "schema" => "%kernel.root_dir%/config/mongo/default.yml",
+        "options" => []
+    ];
+
+    protected function setUp()
+    {
+        $logger = new NullLogger();
+        $this->manager = new DocumentManager($this->config, $logger);
+    }
+
+    protected function tearDown()
+    {
+        $this->manager = null;
+    }
+
+    public function testBarAction()
+    {
+        $this->assertTrue(true);
+    }
+}
